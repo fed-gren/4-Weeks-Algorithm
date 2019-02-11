bc0269c3fd81cacb589a473258f4877c0429fbcf
fed-gren <deveb22@gmail.com>
2019-02-11 23:43:27 +0900

    READMD.md 수정
    풀어볼 문제 분류별로 추가

---
+++ 입출력/B11021.java
@@ -0,0 +1,16 @@
+/**
+ * B11021
+ */
+import java.util.Scanner;
+public class B11021 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();  //test case.
+        for(int i=1; i<=tc; i+=1) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println("Case #"+i + ": " + (a + b));
+        }
+    }
+}
