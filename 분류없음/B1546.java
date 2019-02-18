08e9b46ad3fd23024e4352496fd4c9162cb16fa4
fed-gren <deveb22@gmail.com>
2019-02-18 15:41:24 +0900

    B1546 충돌 수정

--- 분류없음/B1546.java
+++ 분류없음/B1546.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.util.Scanner;
 /**
  * B1546
@@ -20,31 +19,5 @@ public class B1546 {
             sum += arrScore[i];
         }
         System.out.println(sum/(double)numSubject);
-=======
-/**
- * B1546
- */
-import java.util.Scanner;
-public class B1546 {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] score = new int[n];
-        int max = 0;
-        double avg = 0;
-        for(int i =0; i<n; i+=1) {
-            score[i] = sc.nextInt();
-            if(max < score[i]) max = score[i];
-        }
-        double[] newScore = new double[n];
-        double sum = 0;
-        for(int i=0; i<n; i+=1) {
-            newScore[i] = (double)score[i] / (double)max * 100;
-            sum += newScore[i];
-        }
-        avg = sum / (double)n;
-        System.out.println(avg);
->>>>>>> 4a9eb4084541c01c556de59050b9eee41ab3148f
     }
 }
