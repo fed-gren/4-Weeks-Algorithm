24049a5d41cfaad4542c23401a790e0e850d1511
fed-gren <deveb22@gmail.com>
2019-02-22 16:31:36 +0900

    2193 이친수 문제 소스코드 추가

--- DP/B2193.java
+++ DP/B2193.java
@@ -1,19 +1,18 @@
 import java.util.Scanner;
-
 /**
  * B2193
  * 이친수 찾기.
  */
 public class B2193 {
-    static int[] DP;    //길이가 n일 때 이친수의 갯수 저장.
+    static long[] DP;    //길이가 n일 때 이친수의 갯수 저장, 피보나치 수열! 90번째는 int로 불가.
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         sc.close();
-        DP = new int[91];
+        DP = new long[n+1];
+        DP[0] = 0;
         DP[1] = 1;
-        DP[2] = 1;
-        for(int i=3; i<=n; i+=1) {
+        for(int i=2; i<=n; i+=1) {
             DP[i] = DP[i-1] + DP[i-2];
         }
         System.out.println(DP[n]);
