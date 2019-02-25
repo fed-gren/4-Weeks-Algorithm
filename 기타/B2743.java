5c935705160145bf8be8492800d39f7ce2c3238f
fed-gren <deveb22@gmail.com>
2019-02-25 14:03:31 +0900

    2743 단어길이재기 문제 소스코드 추가

---
+++ 기타/B2743.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * B2743
+ */
+public class B2743 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String[] arrStr = str.split("");
+        System.out.println(arrStr.length);
+    }
+}
