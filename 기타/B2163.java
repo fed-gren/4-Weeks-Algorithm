a8667adddddf918c00d18065082c26acc7d8891b
fed-gren <deveb22@gmail.com>
2019-02-26 16:19:47 +0900

    2163 초콜릿 자르기 문제 소스코드 추가

---
+++ 기타/B2163.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * B2163
+ */
+public class B2163 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        sc.close();
+        System.out.println(n*m-1);
+    }
+}
